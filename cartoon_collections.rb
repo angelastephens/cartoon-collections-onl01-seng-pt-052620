e8d1b5fb45cb09e811f9f390ca0969559484990a
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 02:30:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-    veggies.collect do |name|
-     name.upcase + '!'}
-  end
+    veggies.collect { |name| name.upcase + '!'}
 end
 
 
