be585e412e0b91e7cd6c6646c9d39bdb6a0648c4
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 02:44:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
   
 def find_the_cheese(no_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  no_cheese.none? { |cheese| cheese.include}
+  no_cheese.include? { |cheese| cheese.include?}
 end
